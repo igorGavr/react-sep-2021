f8d3144ec275586ba1132b87d16d7114666aa171
Vitaliy Demchyshyn <demchyshyn.v87@gmail.com>
2022-01-19 22:19:58 +0200

    cons2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,40 @@
+import {Routes, Route, Navigate} from "react-router-dom";
+import {Layout} from "./components";
+import {
+    AlbumsPage, PhotosPage,
+    PostCommentsPage,
+    PostDetailsPage,
+    PostsPage,
+    UserDetailsPage,
+    UserPostsPage,
+    UsersPage
+} from "./pages";
+
 const App = () => {
+
   return (
-      <div>
-        App
-      </div>
+      <Routes>
+          <Route path={'/'} element={<Layout/>}>
+              <Route index element={<Navigate to={'users'}/>}/>
+
+              <Route path={'users'} element={<UsersPage/>}>
+                  <Route path={':id'} element={<UserDetailsPage/>}>
+                      <Route path={'posts'} element={<UserPostsPage/>}/>
+                  </Route>
+
+                  <Route path={':id/albums'} element={<AlbumsPage/>}>
+                      <Route path={':albumId/photos'} element={<PhotosPage/>}/>
+                  </Route>
+              </Route>
+
+              <Route path={'posts'} element={<PostsPage/>}>
+                  <Route path={':id'} element={<PostDetailsPage/>}>
+                      <Route path={'comments'} element={<PostCommentsPage/>}/>
+                  </Route>
+              </Route>
+
+          </Route>
+      </Routes>
   );
 };
 
--- src/axios.service.js
+++
@@ -1,3 +0,0 @@
-import axios from 'axios'
-
-export const axiosService = axios.create({baseURL:'dddddddddddddsdfssf'});
