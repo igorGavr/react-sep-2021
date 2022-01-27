0861fa162d42a60fe822e90ac04d3a1bd15c0fa2
Vitaliy Demchyshyn <demchyshyn.v87@gmail.com>
2022-01-27 12:55:40 +0200

    useReducer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
+import {useReducer} from "react";
+
+const reducer = (state, action) => {
+    state = {...state, count2: state.count2 + 1}
+    switch (action.type) {
+        case 'inc':
+            return {...state, count1: state.count1 + 1}
+        case 'dec':
+            return {...state, count1: state.count1 - 1}
+        case 'reset':
+            return {...state, count1: action.payload}
+        default:
+            throw new Error('MyError')
+    }
+}
 
 function App() {
-  return (
-    <>
-    </>
-  );
+    const [state, dispatch] = useReducer(reducer, {count1: 0, count2: 0});
+    const click = () => {
+        dispatch({type: 'inc'})
+    }
+    return (
+        <div>
+            <div>{state.count1}</div>
+            <div>{state.count2}</div>
+            <button onClick={click}>INC</button>
+            <button onClick={() => dispatch({type: 'dec'})}>DEC</button>
+            <button onClick={() => dispatch({type: 'reset4', payload: 8})}>RESET</button>
+        </div>
+    );
 }
 
 export default App;
