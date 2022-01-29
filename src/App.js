d40c7e449c3d57c2426cbe2407038d1440a3a063
Vitaliy Demchyshyn <demchyshyn.v87@gmail.com>
2022-01-29 21:57:52 +0200

    base

--- src/App.js
+++ src/App.js
@@ -1,34 +1,9 @@
-import Form from "./components/Form";
-import Form2 from "./components/Form2";
-import {useEffect, useState} from "react";
-import {set} from "react-hook-form";
-
-function App() {
-
-    let [counter, setCounter] = useState({val: 0});
-
-    useEffect(() => {
-        console.log('use effect');
-        return () => {
-            console.log('safdhgafsd')
-        }
-    }, [counter]);
+const App = () => {
     return (
         <div>
-            <button onClick={
-                () => {
-                    counter.val++;
-                    setCounter({...counter});
-                }
-            }>
-                increment {counter.val}
-            </button>
-
-            {/*<Form/>*/}
-            {/*<hr/>*/}
-            {/*<Form2/>*/}
+            App
         </div>
     );
-}
+};
 
 export default App;
